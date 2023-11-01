0113ac7a8d55e06d846466dca7dfe9b5bd085995
harsha-commit <harshavardhan.bashavathini@gmail.com>
2023-11-01 16:52:08 +0530

    Add Spring Boot First Project

---
+++ spring-boot-first-app/src/test/java/com/spring/boot/springbootfirstapp/SpringBootFirstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot.springbootfirstapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFirstAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
